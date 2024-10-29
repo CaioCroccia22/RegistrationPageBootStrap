49a580554edc1dd4e7132dc015dd36da631243ca
caiocroccia22 <caiocroccia22@gmail.com>
2024-10-29 20:58:20 -0300

    feat: add background and validation style

--- script.js
+++ script.js
@@ -21,9 +21,23 @@ $(document).ready(function(){
                 required: "Please, enter with your e-mail"
             }
         },
+        errorClass: 'is-invalid',
+        validClass: 'is-valid',
+        errorElement: 'div',
+        highlight: function(e){
+            $(e).addClass('is-invalid').removeClass('is-valid');
+        },
+        unhighlight: function(e){
+            $(e).addClass('is-valid').removeClass('is-invalid');
+        },
+        errorPlacement: function( error, e) {
+            error.addClass("invalid-feedback");
+            error.insertAfter(e)
+        },
         submitHandler: function (form) {
             alert("Congratulations");
             form.reset();
+            $('is-valid')
         },
         invalidHandler: function (event, validator) {
             alert("Error, please enter with all fields");

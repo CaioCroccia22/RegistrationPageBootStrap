9648381cd10b1bddd25beb62d4cecdc85d1cbe41
caiocroccia22 <caiocroccia22@gmail.com>
2024-10-29 21:13:33 -0300

    feat: validation alert display

--- script.js
+++ script.js
@@ -21,26 +21,34 @@ $(document).ready(function(){
                 required: "Please, enter with your e-mail"
             }
         },
-        errorClass: 'is-invalid',
-        validClass: 'is-valid',
-        errorElement: 'div',
+        errorClass: "is-invalid", //Classe BootStrap para campos invalidos
+        validClass: "is-valid", //Classe BootStrap para campos validos
+        errorElement: "div", //Exibe Mensagem de erro na div abaixo do campo
         highlight: function(e){
-            $(e).addClass('is-invalid').removeClass('is-valid');
+            $(e).addClass("is-invalid").removeClass("is-valid"); //Adiciona a classe de erro
         },
         unhighlight: function(e){
-            $(e).addClass('is-valid').removeClass('is-invalid');
+            $(e).addClass("is-valid").removeClass("is-invalid"); //Remove classe de erro
         },
-        errorPlacement: function( error, e) {
+        errorPlacement: function(error, e) {
             error.addClass("invalid-feedback");
             error.insertAfter(e)
         },
         submitHandler: function (form) {
-            alert("Congratulations");
+            $('.alert-container').html(`
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Success!</strong> Form submitted successfully.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `);
             form.reset();
-            $('is-valid')
+            $(".is-valid").removeClass("is-valid");
         },
         invalidHandler: function (event, validator) {
-            alert("Error, please enter with all fields");
+            $('.alert-container').html(`<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Error!</strong> Please complete all required fields.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>`); //Chama a classe alert-container e exibe o alert estilizado do css
         }
     })
 })
